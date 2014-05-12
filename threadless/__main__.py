2ec725800392bff6983a15bf99c1dfb9cf23d232
Rob Hoelz <rob@hoelz.ro>
2014-05-11 19:05:20 -0500

    Add simple game logic

--- threadless/__main__.py
+++ threadless/__main__.py
@@ -1,6 +1,37 @@
+from __future__ import print_function
+
+from datetime import datetime
+import time
+
+class Game(object):
+    FRAME_RATE = 60
+
+    def __init__(self):
+        self.tickers = []
+
+    def draw(self):
+        pass
+
+    def run(self):
+        sec_per_frame = 1 / 60.0
+        is_running   = True
+
+        start = datetime.now()
+        while is_running:
+            self.draw()
+            for ticker in self.tickers:
+                ticker()
+
+            end       = datetime.now()
+            delta     = end - start
+            wait_time = sec_per_frame - (end - start).microseconds * 1000000
+            if wait_time > 0:
+                time.sleep(wait_time)
+            start = end
 
 def main():
     """ your app starts here
     """
-    import pygame.examples.stars
-    pygame.examples.stars.main()
+
+    game = Game()
+    game.run()
