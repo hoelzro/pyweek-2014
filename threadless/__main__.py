613bb5f13b70a95465543a49aa964e94a7e6bbf8
Rob Hoelz <rob@hoelz.ro>
2014-05-11 20:36:09 -0500

    Start on keybinding system

--- threadless/__main__.py
+++ threadless/__main__.py
@@ -4,13 +4,22 @@ import curses
 from datetime import datetime
 import time
 
-class CursesScreen(object):
+class Screen(object):
+    Q = 1
+
+class CursesScreen(Screen):
+    KEY_MAP = {
+        Screen.Q: ord('q'),
+    }
+
     def __init__(self):
         self.screen = curses.initscr()
         curses.noecho()
         curses.cbreak()
         self.screen.nodelay(1)
         self.screen.keypad(1)
+
+        self.keybindings = {}
 
     def __del__(self):
         curses.nocbreak()
@@ -26,12 +35,28 @@ class CursesScreen(object):
         self.screen.addch(y, x, 'P')
         self.screen.refresh()
 
+    def on_key_down(self, key, callback):
+        key = self.KEY_MAP[key]
+        if key not in self.keybindings:
+            self.keybindings[key] = []
+        self.keybindings[key].append(callback)
+
+    def process_input(self):
+        ch = self.screen.getch()
+
+        if ch != -1:
+            callbacks = self.keybindings.get(ch, [])
+            for cb in callbacks:
+                cb()
+
 class Game(object):
     FRAME_RATE = 60
 
     def __init__(self):
         self.tickers = []
         self.screen = CursesScreen()
+
+        self.tickers.append(self.screen.process_input)
 
     def run(self):
         sec_per_frame = 1 / 60.0
